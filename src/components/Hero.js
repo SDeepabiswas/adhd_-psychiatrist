850a27f397871caf46bdf2a0a44aa187536bf295
sayandeepa <biswassayandeepa@gmail.com>
2023-12-27 15:57:43 +0530

    third commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,30 @@
 import React from "react";
 import Typed from 'react-typed'
+
+const DrawOutlineButton = ({ children, ...rest }) => {
+  return (
+    <button
+      {...rest}
+      className="group relative rounded-lg px-4 py-2 mx-auto font-bold bg-[#61e1b9] w-[140px] text-black transition-colors duration-[400ms] hover:text-green-900"
+    >
+      <span>{children}</span>
+
+      {/* TOP */}
+      <span className="absolute left-0 top-0 h-[2px] w-0 bg-green-900 transition-all duration-1 group-hover:w-full" />
+
+      {/* RIGHT */}
+      <span className="absolute right-0 top-0 h-0 w-[2px] bg-green-900 duration-1 group-hover:h-full" />
+
+      {/* BOTTOM */}
+      <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-green-900 transition-all delay-200 duration-1 group-hover:w-full" />
+
+      {/* LEFT */}
+      <span className="absolute bottom-0 left-0 h-0 w-[2px] bg-green-800 transition-all delay-300 duration-1 group-hover:h-full" />
+    </button>
+  );
+};
+
+
 const Hero=() => {
     return (
         <div className="text-white">
@@ -16,7 +41,8 @@ const Hero=() => {
                     loop/>
                 </div>
                 <p className="my-4 font-serif text-lg">Elevate your practice: connect with new clients and effortlessly confirm appointments here, maximizing your focus on client care.</p>
-                <button className="bg-[#00df9a] w-[140px] rounded-lg text-md font-bold my-6 mx-auto py-3 text-black">Get Started</button>
+                <DrawOutlineButton>Get Started</DrawOutlineButton>
+                {/* <button className="bg-[#00df9a] w-[140px] rounded-lg text-md font-bold my-6 mx-auto py-3 text-black">Get Started</button> */}
             </div>
             </div>
     )
